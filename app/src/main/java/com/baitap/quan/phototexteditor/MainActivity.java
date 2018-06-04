b474e38930efaa47b2b4383662e002820fe0aa52
dquand91 <dquand91@gmail.com>
2018-06-04 14:20:21 +0700

    - Update + Delete double tap to Edit Text field + on tap show/hide navigator bar + disable button save + double tap zoom in or zoom out

--- app/src/main/java/com/baitap/quan/phototexteditor/MainActivity.java
+++ app/src/main/java/com/baitap/quan/phototexteditor/MainActivity.java
@@ -1,42 +1,35 @@
 package com.baitap.quan.phototexteditor;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-import com.baitap.quan.photoeditor.OnPhotoEditorListener;
+import com.baitap.quan.photoeditor.CustomImageView.OnSingleTapListener;
+import com.baitap.quan.photoeditor.CustomImageView.ZoomageView;
 import com.baitap.quan.photoeditor.PhotoEditor;
 import com.baitap.quan.photoeditor.PhotoEditorView;
-import com.baitap.quan.photoeditor.ViewType;
 import com.baitap.quan.photoeditor.ZoomLayout;
 
-public class MainActivity extends AppCompatActivity implements OnPhotoEditorListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private PhotoEditor mPhotoEditor;
     private PhotoEditorView mPhotoEditorView;
     private ZoomLayout zoomlayoutGit;
+    private ZoomageView zoomImageView;
+    private View bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_main_screen);
 
-        setContentView(R.layout.activity_main);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         initViews();
-
-        mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
-                .setPinchTextScalable(true)
-                .build();
-        mPhotoEditorView.getSource().setImageDrawable(getResources().getDrawable(R.drawable.img_src));
-        mPhotoEditor.setOnPhotoEditorListener(this);
-        mPhotoEditor.addText("Hello Quan", ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
     }
 
     private void initViews() {
@@ -44,44 +37,24 @@ public class MainActivity extends AppCompatActivity implements OnPhotoEditorList
         TextView btnSave;
         TextView btnCancel;
 
-        zoomlayoutGit = findViewById(R.id.zoomRelativeLayout);
+        bottomBar = findViewById(R.id.constraintTools_first);
 
-        zoomlayoutGit.post(new Runnable() {
+        zoomImageView = findViewById(R.id.zoomImageView_first);
+        zoomImageView.setImageDrawable(getResources().getDrawable(R.drawable.img_src));
+
+        zoomImageView.setOnSingleTapListener(new OnSingleTapListener() {
             @Override
-            public void run() {
-                int width = zoomlayoutGit.getMeasuredWidth();
-                int height = zoomlayoutGit.getMeasuredHeight();
-                zoomlayoutGit.setContentSize(width, height);
+            public void onSingleTap() {
+                if(bottomBar.getVisibility() == View.VISIBLE){
+                    bottomBar.setVisibility(View.GONE);
+                } else {
+                    bottomBar.setVisibility(View.VISIBLE);
+                }
             }
         });
-        mPhotoEditorView = (PhotoEditorView) findViewById(R.id.photoEditorView);
 
         btnSave = findViewById(R.id.btnSave_ActionBar);
         btnCancel = findViewById(R.id.btnCancel_ActionBar);
-    }
-
-    @Override
-    public void onEditTextChangeListener(View rootView, String text, int colorCode) {
-
-    }
-
-    @Override
-    public void onAddViewListener(ViewType viewType, int numberOfAddedViews) {
-
-    }
-
-    @Override
-    public void onRemoveViewListener(int numberOfAddedViews) {
-
-    }
-
-    @Override
-    public void onStartViewChangeListener(ViewType viewType) {
-
-    }
-
-    @Override
-    public void onStopViewChangeListener(ViewType viewType) {
-
+        btnSave.setEnabled(false);
     }
 }
---
+++ photoeditor/src/main/java/com/baitap/quan/photoeditor/CustomImageView/OnSingleTapListener.java
@@ -0,0 +1,6 @@
+package com.baitap.quan.photoeditor.CustomImageView;
+
+public interface OnSingleTapListener {
+
+	void onSingleTap();
+}
